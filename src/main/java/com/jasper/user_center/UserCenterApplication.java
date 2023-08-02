173d02a86b68d6797857e003db2ab01379573b40
Jasper <2251747537@qq.com>
2023-08-03 00:28:23 +0800

    swagger

--- src/main/java/com/jasper/user_center/UserCenterApplication.java
+++ src/main/java/com/jasper/user_center/UserCenterApplication.java
@@ -3,9 +3,8 @@ package com.jasper.user_center;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+
 @SpringBootApplication
 @MapperScan("com.jasper.user_center.mapper")
 public class UserCenterApplication {
---
+++ src/main/java/com/jasper/user_center/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.jasper.user_center.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域请求的域名
+                .allowedOrigins("https://localhost:8000.com")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
