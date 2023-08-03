97b868dcf3208201af7967c3ee8279e0c127b317
Jasper <2251747537@qq.com>
2023-08-04 01:09:03 +0800

    swagger

--- src/main/java/com/jasper/user_center/config/SwaggerConfig.java
+++ src/main/java/com/jasper/user_center/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.jasper.user_center.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableSwagger2WebMvc
+@Profile("dev")
 public class SwaggerConfig {//对于配置类要求可以看懂即可，不用反复去写，将来可以CV
     //配置Swagger2的Docket的Bean实例
 
